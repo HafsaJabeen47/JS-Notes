952e2912d26b9e97e8bb86dbd92aa7efd0212d7b
Hafsa <hafsajabeen47@gmail.com>
2024-12-04 12:13:20 +0500

    Array Method: indexOf()

--- 01-JavaScript Lectures Notes By Me/Lec-5-Arrays/Lec-5.1-Arrays-Method.js
+++ 01-JavaScript Lectures Notes By Me/Lec-5-Arrays/Lec-5.1-Arrays-Method.js
@@ -1,1 +1,11 @@
+// Arrays New Methods Explored By Me from W3School:
 
+//&& 1: array.indexOf();
+// The indexOf() method searches an array for an element value and returns its position.
+
+let fruits = ['apple', 'mango', 'orange', 'pineApple'];
+let findPosition = fruits.indexOf('mango');
+
+console.log(findPosition) // mango is at 1 position og array
+
+//&& 2:
